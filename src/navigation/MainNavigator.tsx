9e4575b878a516382b1170c2412a13e9923e3be6
WellingtonADS <ads231n2@gmail.com>
2025-08-25 10:37:17 -0400

    feat: Substituir tela de lista de pontos pela tela de histórico de pontos na navegação

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View, Text } from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
+import PointHistoryScreen from '../screens/PointHistoryScreen';
 
 // Telas que ainda vamos criar. Usamos componentes temporários para evitar erros.
 const PointListScreen = () => <View><Text>Lista de Pontos</Text></View>;
@@ -12,7 +13,7 @@ const EditProfileScreen = () => <View><Text>Editar Perfil</Text></View>;
 // Define o tipo para os parâmetros de cada tela
 export type MainStackParamList = {
   HomePage: undefined;
-  PointListScreen: undefined;
+  PointHistoryScreen: undefined;
   PointDetailScreen: { companyId: number }; // Exemplo de parâmetro
   EditProfileScreen: undefined;
 };
@@ -23,7 +24,7 @@ const MainNavigator = () => {
   return (
     <MainStack.Navigator screenOptions={{ headerShown: false }}>
       <MainStack.Screen name="HomePage" component={HomeScreen} />
-      <MainStack.Screen name="PointListScreen" component={PointListScreen} />
+      <MainStack.Screen name="PointHistoryScreen" component={PointHistoryScreen} />
       <MainStack.Screen name="PointDetailScreen" component={PointDetailScreen} />
       <MainStack.Screen name="EditProfileScreen" component={EditProfileScreen} />
     </MainStack.Navigator>
