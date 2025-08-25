10e0a317de40f941bdcacfb2a9c9107c67d087e0
WellingtonADS <ads231n2@gmail.com>
2025-08-25 09:35:28 -0400

    feat: Adicionar configuração de linking para navegação e mapear tela de redefinição de senha

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer,LinkingOptions } from '@react-navigation/native';
 import { AuthProvider } from './src/context/AuthContext';
 import AppNavigator from './src/navigation/AppNavigator';
+// 2. Corrigir o caminho da importação para AuthNavigator
+import { AuthStackParamList } from './src/navigation/AuthNavigator';
+
+// 1. Defina a configuração do linking
+const linking: LinkingOptions<AuthStackParamList> = {
+  prefixes: ['fidelizacliente://'],
+  config: {
+    screens: {
+      // Mapeia o caminho do URL para o nome do ecrã no seu navegador
+      ResetPassword: 'reset-password',
+    },
+  },
+};
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       <AuthProvider>
         <AppNavigator />
       </AuthProvider>
